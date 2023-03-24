9cff5150ae65507244a67afc22834d166f82cf7f
jhansireddy34 <125592388+jhansireddy34@users.noreply.github.com>
2023-03-24 14:14:00 +0000

    second commit

--- jhansi-sollati/src/components/profile/profile.js
+++ jhansi-sollati/src/components/profile/profile.js
@@ -14,10 +14,16 @@ const Profile = (props)=>{
         <ProfileAppBar />
         </Grid>
 
-        <Grid xs={12}md={3}item={true}>
-            
-        <ProfileCard name="Jhansi Sollati"desc="Aspiring Developer"notes="React/Flutter/Python"imageUrl="https://i.pinimg.com/280x280_RS/d7/b5/21/d7b5212f77583ae60aa05feb2844aae9.jpg" />
-        
+        <Grid xs={12}md={4}item={true}>
+        <Grid container spacing={2}>
+        <Grid xs={4} item={true}>
+              
+        <img src="https://i.pinimg.com/280x280_RS/d7/b5/21/d7b5212f77583ae60aa05feb2844aae9.jpg" />
+        </Grid>
+        <Grid xs={8} item={true}>
+        <ProfileCard name="Jhansi Sollati"desc="Aspiring Developer"notes="React/Flutter/Python" />
+        </Grid>
+        </Grid>
           </Grid >
 
 
